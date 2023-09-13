c463244234c93976d8283bfe9a749c62e0f62930
Bruno H. Homem <brunohhomem@gmail.com>
2023-09-13 18:51:21 -0300

    JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
+function convertPokemonLi(pokemon) {
+  return `
+  <li class="pokemon">
+  <span class="number">#001</span>
+  <span class="name">${pokemon.name}</span>
 
+  <div class="detail">
+    <ol class="types">
+      <li class="type">Grass</li>
+      <li class="type">Poison</li>
+    </ol>
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="${pokemon.name}"
+    />
+  </div>
+</li>
+  
+  `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeAPI.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML += pokemons.map(convertPokemonLi).join('')
+})
